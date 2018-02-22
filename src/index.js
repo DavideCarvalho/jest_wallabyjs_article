774f9d6f840dcb09c310e1ced2d2ff8fd5e14418
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2018-02-21 22:03:42 -0300

    taking off unnecessary code and files to template.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-const axios = require('axios');
-const somaDoisNumeros = (a, b) => a+b;
-const pegaPost = async (postNumber)=> await axios.get(`https://jsonplaceholder.typicode.com/posts/${postNumber}`);
-
 const api = {
-  somaDoisNumeros,
-  pegaPost
 }
 
 module.exports = api;
