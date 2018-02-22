c9e369365bcb70535a7d94850bb84c42c48beaff
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2018-02-21 23:24:00 -0300

    final commit

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,17 +1,26 @@
-const api = require('../index');
-const axios = require('axios');
+const api = require("../index");
+const axios = require("axios");
 
-describe('index.js', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    expect(api.somaDoisNumeros(1,2)).toMatchSnapshot();
-  })
-  
-  it('expect to mock axios request', async () => {
-    const spy = jest.spyOn(axios, 'get');
-    spy.mockReturnValue('MockedValue');
+const mockedAxios = {
+  data: {
+    userId: 1,
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body:
+      "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  }
+};
+
+describe("index.js", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(api.somaDoisNumeros(1, 2)).toMatchSnapshot();
+  });
+
+  it("expect to mock axios request", async () => {
+    const spy = jest.spyOn(axios, "get");
+    spy.mockReturnValue(mockedAxios);
     const apiResponse = await api.pegaPost(1);
-    expect(apiResponse).toBe('MockedValue');
-    expect(spy).toHaveBeenCalled();
-  })
-})
-
+    expect(apiResponse).toMatchSnapshot();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 const somaDoisNumeros = (a, b) => a+b;
-const pegaPost = async (postNumber)=> await axios.get(`https://jsonplaceholder.typicode.com/posts/${postNumber}`);
+const pegaPost = async (postNumber)=> {
+  const apiResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postNumber}`);
+  return apiResponse.data.body.split('').reverse().join('');
+}
 
 const api = {
   somaDoisNumeros,
